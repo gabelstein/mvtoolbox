a19b4d8240fb2e7abae2e8124833283bf11e8255
gabelstein <darkmash@web.de>
2021-12-30 12:08:33 +0100

    jetzt?

--- setup.py
+++ setup.py
@@ -1,30 +1,12 @@
-import os.path as op
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-
-# get the version (don't import mne here, so dependencies are not needed)
-version = '0.1'
-
-
-setup(name='mvtoolbox',
-      version=version,
-      description='toolboc for multivariate data analysis',
-      url='https://pyriemann.readthedocs.io',
-      author='Goofy',
-      author_email='gabriel@bccn-berlin.de',
-      license='BSD (3-clause)',
-      packages=[''],
-      long_description_content_type='text/markdown',
-      project_urls={
-          'Documentation': 'https://pyriemann.readthedocs.io',
-          'Source': 'https://github.com/pyRiemann/pyRiemann',
-          'Tracker': 'https://github.com/pyRiemann/pyRiemann/issues/',
-      },
-      platforms='any',
-      python_requires=">=3.6",
-      install_requires=['numpy', 'scipy', 'scikit-learn',  'joblib', 'pandas'],
-      extras_require={'docs': ['sphinx-gallery', 'sphinx-bootstrap_theme', 'numpydoc', 'mne', 'seaborn'],
-                      'tests': ['pytest', 'seaborn', 'flake8']},
-      zip_safe=False,
+setup(
+    name='mvtoolbox',
+    version='0.0.1',
+    packages=['mvtoolbox', 'mvtoolbox.preprocessing'],
+    url='',
+    license='',
+    author='Gabriel',
+    author_email='',
+    description=''
 )
