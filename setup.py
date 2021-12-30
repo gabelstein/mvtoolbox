2d33fa92fe252886146c177ea383b3ba78f4c174
gabelstein <darkmash@web.de>
2021-12-30 11:28:05 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mvtoolbox',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Gabriel',
+    author_email='',
+    description=''
+)
