8ab593bbfc81ae01336ecd02939c1d1bfe651bc8
gabelstein <darkmash@web.de>
2021-12-30 11:29:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mvtoolbox',
     version='0.0.1',
-    packages=[''],
+    packages=find_packages(),
     url='',
     license='',
     author='Gabriel',
